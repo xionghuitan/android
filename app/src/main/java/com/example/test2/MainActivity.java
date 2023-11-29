1b0b44a5be50e091326b9009970830250d8b8380
xionghui_glb <3069456697@qq.com>
2023-11-29 21:09:03 +0800

    按钮弹出一个按钮

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.test2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String serverIP = "192.168.10.102";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // 测试注解
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button1);
+        // Client client = new Client();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Hello hello = client.hello;
+                Toast.makeText(MainActivity.this, "Button 1", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
