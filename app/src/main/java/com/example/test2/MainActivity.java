b9668b43fee3905beb8259c8e78a14996b840867
xionghui_glb <3069456697@qq.com>
2023-11-29 20:54:04 +0800

    测试android studio

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -1,28 +1,16 @@
 package com.example.test2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import java.util.concurrent.Callable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String serverIP = "192.168.10.102";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // 测试注解
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button1);
-        Client client = new Client();
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Hello hello = client.hello;
-            }
-        });
     }
 }
